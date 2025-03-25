206d3b986eba4b4e2d584b3483352e27d66ae386
manohar-hash <245122749005@mvsrec.edu.in>
2025-03-25 14:52:52 +0530

    Add files via upload

---
+++ arith.java
@@ -0,0 +1,16 @@
+public class arith{
+	public static void main(String[] args){
+		int a=5;
+		int b=6;
+	      	int r1=a+b;
+		int r2=a-b;
+		int r3=a*b;
+		float r4=a/b;
+		int r5=a%b;
+		System.out.println("addition="+r1);
+		System.out.println("subtraction="+r2);
+		System.out.println("multiplication="+r3);
+		System.out.println("division"+r4);
+		System.out.println("modulus"+r5);
+	}
+}
